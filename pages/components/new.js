5e25a984b9d94debc444488d11b4fdbdcb68ae3b
askuahwah <askuahwah@gmail.com>
2023-12-14 00:12:46 +0530

    dashboard

--- pages/components/new.js
+++ pages/components/new.js
@@ -2,6 +2,13 @@ import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import Menu from "./svg/menu_icon.svg";
 import styles from '../../styles/mobileNav.module.css';
+import DashboardIcon from './svg/dashboard.svg';
+import AnalyticsIcon from './svg/anaytics.svg';
+import PostsIcon from './svg/dashboard.svg';
+import FavoritesIcon from './svg/favorite_nav.svg';
+import LineupIcon from './svg/lineup.svg';
+import Profile from './svg/profile.svg';
+import { stringifyError } from 'next/dist/shared/lib/utils';
 
 const Navbar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -35,12 +42,16 @@ const Navbar = () => {
         <Menu/>
       </div>
       <div ref={sidebarRef} className={`${styles.sidebar} ${sidebarOpen ? styles.open : ''}`}>
+      <div className={styles.profile}>
+        <div><Profile/></div>
+        <p>Origin</p>
+      </div>
         <ul>
-          <li><Link href="/" onClick={closeSidebar}>Dashboard</Link></li>
-          <li><Link href="/favorites" onClick={closeSidebar}>Favorites</Link></li>
-          <li><Link href="/posts" onClick={closeSidebar}>Posts</Link></li>
-          <li><Link href="/analytics" onClick={closeSidebar}>Analytics</Link></li>
-          <li><Link href="/lineup" onClick={closeSidebar}>Line-up Makers</Link></li>
+          <li><Link href="/" onClick={closeSidebar}><div className={styles.icon}><DashboardIcon/><p>Dashboard</p></div></Link></li>
+          <li><Link href="/favorites" onClick={closeSidebar}><div className={styles.icon}><FavoritesIcon/><p>Favorites</p></div></Link></li>
+          <li><Link href="/posts" onClick={closeSidebar}><div className={styles.icon}><PostsIcon/><p>Posts</p></div></Link></li>
+          <li><Link href="/analytics" onClick={closeSidebar}><div className={styles.icon}><AnalyticsIcon/><p>Analytics</p></div></Link></li>
+          <li><Link href="/lineup" onClick={closeSidebar}><div className={styles.icon}><LineupIcon/><p>Line-up Maker</p></div></Link></li>
         </ul>
       </div>
     </div>
